801e019779e3609e22d938905c220a49ee461ec9
PN <pnikolaykosev@gmail.com>
2020-02-16 13:37:29 +0100

    Added HttpCookie/Impl

---
+++ 01.HTTPExercise/src/javache/http/impl/HttpCookieImpl.java
@@ -0,0 +1,28 @@
+package javache.http.impl;
+
+import javache.http.HttpCookie;
+
+public class HttpCookieImpl implements HttpCookie {
+    private String name;
+    private String value;
+
+    public HttpCookieImpl(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s=%s", this.name, this.value);
+    }
+}
